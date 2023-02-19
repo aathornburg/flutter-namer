e0425f65e58ba1506168c6764353d2d2a70b392d
Alan Thornburg <alan.thornb@gmail.com>
2023-02-19 15:41:02 -0500

    Added navigation panel on left and set state with it

--- lib/main.dart
+++ lib/main.dart
@@ -45,7 +45,54 @@ class MyAppState extends ChangeNotifier {
   }
 }
 
-class MyHomePage extends StatelessWidget {
+class MyHomePage extends StatefulWidget {
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+
+  var selectedIndex = 0;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Row(
+        children: [
+          SafeArea(
+            child: NavigationRail(
+              extended: false,
+              destinations: [
+                NavigationRailDestination(
+                    icon: Icon(Icons.home),
+                    label: Text('Home')
+                ),
+                NavigationRailDestination(
+                  icon: Icon(Icons.favorite),
+                  label: Text('Favorites')
+                )
+              ],
+              selectedIndex: selectedIndex,
+              onDestinationSelected: (value) {
+                setState(() {
+                  selectedIndex = value;
+                });
+              },
+            )
+          ),
+          Expanded(
+            child: Container(
+              color: Theme.of(context).colorScheme.primaryContainer,
+              child: GeneratorPage()
+            )
+          )
+        ]
+      )
+    );
+  }
+}
+
+class GeneratorPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     var appState = context.watch<MyAppState>();
@@ -58,41 +105,39 @@ class MyHomePage extends StatelessWidget {
       icon = Icons.favorite_border;
     }
 
-    return Scaffold(
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            BigCard(pair: pair),
-            SizedBox(height: 10),
-            Row(
-                // mainAxisAlignment: MainAxisAlignment.center,
-                mainAxisSize: MainAxisSize.min,  // This does the same as above in this use case
-                children: [
-                  ElevatedButton.icon(
-                      onPressed: () {
-                        appState.toggleFavorite();
-                      },
-                      icon: Icon(icon),
-                      label: Text('Like')
-                  ),
-                  SizedBox(width: 10),
-                  ElevatedButton(
-                      onPressed: () {
-                        appState.getNext();
-                      },
-                      child: Text('Next')
-                  )
-                ]
-            ),
-            SizedBox(height: 10),
-            Row(
+    return Center(
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          BigCard(pair: pair),
+          SizedBox(height: 10),
+          Row(
+              // mainAxisAlignment: MainAxisAlignment.center,
+              mainAxisSize: MainAxisSize.min,  // This does the same as above in this use case
               children: [
-                Text(appState.favorites.toString())
+                ElevatedButton.icon(
+                    onPressed: () {
+                      appState.toggleFavorite();
+                    },
+                    icon: Icon(icon),
+                    label: Text('Like')
+                ),
+                SizedBox(width: 10),
+                ElevatedButton(
+                    onPressed: () {
+                      appState.getNext();
+                    },
+                    child: Text('Next')
+                )
               ]
-            )
-          ]
-        ),
+          ),
+          SizedBox(height: 10),
+          Row(
+            children: [
+              Text(appState.favorites.toString())
+            ]
+          )
+        ]
       )
     );
   }
